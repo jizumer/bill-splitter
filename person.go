1801462421213fe26d3e9fa0681443b03db1d5ff
Juan Infante Zumer <jizumer@gmail.com>
2024-05-11 12:06:51 +0200

    feat: load all persons from file

--- person.go
+++ person.go
@@ -1,78 +1,32 @@
 package main
 
 import (
-	"errors"
-
-	_ "github.com/mattn/go-sqlite3"
+	"encoding/json"
+	"log"
+	"os"
 )
 
 type Person struct {
-	ID      int     `json:"id,omitempty"`
 	Name    string  `json:"name"`
 	Balance float64 `json:"balance"`
 }
 
-func (p *Person) Create() error {
-	db := GetConnection()
-	defer db.Close()
-
-	stmt, err := db.Prepare("INSERT INTO person(name, balance) values(?, ?)")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	r, err := stmt.Exec(p.Name, p.Balance)
-	if err != nil {
-		return err
-	}
-
-	if i, err := r.RowsAffected(); err != nil || i != 1 {
-		return errors.New("ERROR: Se esperaba una fila afectada")
-	}
-
-	return nil
-}
-
 func (p *Person) FindAll() ([]Person, error) {
-	db := GetConnection()
-	defer db.Close()
 
-	rows, err := db.Query("SELECT * FROM person")
+	file, err := os.Open("persons.json")
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
-	person := []Person{}
-	for rows.Next() {
-		var p Person
-		err := rows.Scan(&p.ID, &p.Name, &p.Balance)
-		if err != nil {
-			return nil, err
-		}
-		person = append(person, p)
-	}
-	return person, nil
-}
+	defer file.Close()
 
-func (p *Person) Update() error {
-	db := GetConnection()
-	defer db.Close()
-
-	stmt, err := db.Prepare("UPDATE person SET name=?, balance=? WHERE id=?")
+	decoder := json.NewDecoder(file)
+	persons := []Person{}
+	err = decoder.Decode(&persons)
 	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	r, err := stmt.Exec(p.Name, p.Balance, p.ID)
-	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if i, err := r.RowsAffected(); err != nil || i != 1 {
-		return errors.New("ERROR: Se esperaba una fila afectada")
-	}
+	log.Println("Persons: ", persons)
+	return persons, nil
 
-	return nil
 }
